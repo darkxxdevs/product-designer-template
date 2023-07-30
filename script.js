f76940e93a5e732a1481db66ccd8a5e5c971436e
arpit <arpit2003y@gmail.com>
2023-07-30 17:04:09 +0530

    added effects

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 function mouseMovement() {
     window.addEventListener("mousemove", (dets) => {
-        document.querySelector("#circle").style.transform = `translate(${dets.clientX}px , ${dets.clientY}px)`
+        document.querySelector(".circle").style.transform = `translate(${dets.clientX}px , ${dets.clientY}px)`
+        document.querySelector("#page-2 .circle").style.transform = `translate(${dets.clientX}px , ${dets.clientY}px)`
     })
 }
 
 mouseMovement();
+
+
+function homePageAnimation() {
+    var t1 = gsap.timeline();
+
+    t1.from(".navbar", {
+        y: '-10',
+        opacity: 0,
+        duration: 2,
+        ease: Expo.easeInOut
+    })
+        .to(".boundingelem", {
+            delay: -2,
+            y: '0%',
+            duration: 2,
+            ease: Expo.easeInOut,
+            stagger: .2
+        })
+
+        .from("#home-footer", {
+            y: - 10,
+            opacity: 0,
+            duration: 1.5,
+            delay: -1,
+            ease: Expo.easeInOut
+
+
+        })
+
+}
+
+homePageAnimation();
